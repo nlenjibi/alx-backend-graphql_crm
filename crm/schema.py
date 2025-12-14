e8858fe4804b65932dcb2d57d623d479781e88f4
NLENJIBI TIMOTHY <69208852+nlenjibi@users.noreply.github.com>
2025-12-14 15:18:58 +0000

    Update schema.py

--- crm/schema.py
+++ crm/schema.py
@@ -216,11 +216,7 @@ class OrderNode(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     hello = graphene.String(default_value="Hello, GraphQL!")
-    all_customers = graphene.List(
-        CustomerType,
-        filter=CustomerFilterInput(),
-        order_by=graphene.String(),
-    )
+    all_customers = graphene.List(CustomerType, filter=CustomerFilterInput(), order_by=graphene.String())
     all_products = DjangoFilterConnectionField(
         ProductNode,
         filter=ProductFilterInput(),
